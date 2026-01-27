150eff5409df92a8e7c156ef58dc200d3a72b468
Indresh-Gupta <indreshgupta125@gmail.com>
2026-01-27 16:06:39 +0530

    Relation with mongodb for diffrent database

---
+++ index.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+main()
+.then(() =>{
+    console.log("connection successful");
+})
+.catch((err) =>console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
+
+const userSchema = new mongoose.Schema({
+    name:String,
+    email:String,
+    age:Number,
+});
+
+  const User= mongoose.model("User", userSchema);
+
+  User.findByIdAndDelete("66728a1431a02f8f523fe20b").then((res) => {
+    console.log(res);
+  });
+
+  // User.updateMany({ age: { $gt:"48"} }, {age:55})
+  // .then((res) =>{
+  //   console.log(res);
+  // })
+  // .catch((err) => {
+  //   console.log(err);
+  // });
+ 
+  // User.insertMany( [
+  //   {name:"Tony", email:"tony@gmail.com", age:50},
+  //   {name:"Peter", email:"peter@gmail.com", age:30},
+  //   {name:"Bruce", email:"bruce@gmail.com",age:47 },
+  // ]).then((res) => {
+  //   console.log(res);
+  //
